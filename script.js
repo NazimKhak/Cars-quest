6e4a28985b416ca8a9c5b7f2d9f93c9bd82b75c2
NazimKhak <122981712+NazimKhak@users.noreply.github.com>
2023-01-25 16:26:11 +0200

    25.01

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const refs = {
   draggble_list: document.querySelector("#draggble-list"),
-  check_button: document.querySelector("check-button"),
+  check_button: document.querySelector(".check-btn"),
+  resetButton: document.querySelector(".reset-btn"),
 };
 
 const richestPeople = [
-  "Jeff Bezos",
-  "Bill Gates",
-  "Warren Buffet",
-  "Bernand Arnault",
-  "Carlos Slim Helu",
-  "Amancio Ortega",
-  "Larry Elison",
-  "Mark Zuckerberg",
-  "Michael Bloomberg",
+  "Koenigsegg Jesko Absolut",
+  "Hennessey Venom F5",
+  "SSC Tuatara",
+  "Bugatti Chiron Super Sport",
+  "Koenigsegg Agera RS",
+  "Hennessey Venom GT",
+  "Bugatti Veyron Super Sport",
+  "Bugatti Chiron",
+  "SSC Ultimate Aero TT",
   "Larry Page",
 ];
 
@@ -29,18 +30,18 @@ function createlist() {
     .forEach((person, index) => {
       const listItem = document.createElement("li");
       listItem.setAttribute("data-index", index);
-      listItem.innerHTML = `<span class='number>${index + 1}</span>
+      listItem.innerHTML = `
+      <span class="number">${index + 1}</span> 
           <div class ="draggable" draggable="true">
           <p class="person-name">${person}</p>
           <i>
-          svg style="width:24px; heigth:24px" viewBox="0 0 24 24">
+          <svg style="width:24px; heigth:24px" viewBox="0 0 24 24">
           <path fill= "currentColor" d="M22 2V4H2V2H22M7 10.5V13.5H17V10.5H7M2 20V22H22V20H22" /<>/<svg>
           </i>
           </div>
           `;
       listItems.push(listItem);
       refs.draggble_list.appendChild(listItem);
-      console.log(listItems);
     });
   addEventListeners();
 }
@@ -49,19 +50,54 @@ function dragStart() {
   dragStartIndex = +this.closest("li").getAttribute("data-index");
 }
 
-function addEventListeners() {
-  const draggables = document.querySelector(".draggable");
-  const dragListItems = document.querySelector(".draggable-list li");
-  draggables.forEach((draggable) => {
-    draggable.addEventListener("dragstart", dragStart);
+function dragEnter() {
+  this.classList.add("over");
+}
+function dragLeave() {
+  this.classList.remove("over");
+}
+function dragDrop(e) {
+  const dragEndIndex = +this.getAttribute("data-index");
+  swapItems(dragStartIndex, dragEndIndex);
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function checkorder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector(".draggable").innerText.trim();
+    if (personName !== richestPeople[index]) {
+      listItem.classList.add("wrong");
+    } else {
+      listItem.classList.remove("wrong");
+      listItem.classList.add("right");
+    }
   });
 }
 
-dragListItems.forEach((item) => {
-  item.addEventListener("dragover", dragOVer);
-  item.addEventListener("drop", dragDrop);
-  item.addEventListener("dragenter", dragEnter);
-  item.addEventListener("dragleave", dragLeave);
-});
+function addEventListeners() {
+  const draggables = document.querySelectorAll(".draggable");
+  const dragListItems = document.querySelectorAll(".draggable-list li");
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", dragStart);
+  });
 
-check.addEventListener("click", checkOrder);
+  dragListItems.forEach((item) => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("drop", dragDrop);
+    item.addEventListener("dragenter", dragEnter);
+    item.addEventListener("dragleave", dragLeave);
+  });
+}
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector(".draggable");
+  const itemTwo = listItems[toIndex].querySelector(".draggable");
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+refs.check_button.addEventListener("click", checkorder);
+refs.resetButton.addEventListener("click", reset);
