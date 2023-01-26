12747dfc1d3647ad301a9568e76456dfcf7f5271
NazimKhak <122981712+NazimKhak@users.noreply.github.com>
2023-01-26 12:03:36 +0200

    26.01

--- script.js
+++ script.js
@@ -27,13 +27,13 @@ function createlist() {
     .map((a) => ({ value: a, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map((a) => a.value)
-    .forEach((person, index) => {
+    .forEach((cars, index) => {
       const listItem = document.createElement("li");
       listItem.setAttribute("data-index", index);
       listItem.innerHTML = `
       <span class="number">${index + 1}</span> 
           <div class ="draggable" draggable="true">
-          <p class="person-name">${person}</p>
+          <p class="car-name">${cars}</p>
           <i>
           <svg style="width:24px; heigth:24px" viewBox="0 0 24 24">
           <path fill= "currentColor" d="M22 2V4H2V2H22M7 10.5V13.5H17V10.5H7M2 20V22H22V20H22" /<>/<svg>
@@ -66,8 +66,8 @@ function dragOver(e) {
 
 function checkorder() {
   listItems.forEach((listItem, index) => {
-    const personName = listItem.querySelector(".draggable").innerText.trim();
-    if (personName !== richestPeople[index]) {
+    const carName = listItem.querySelector(".draggable").innerText.trim();
+    if (carName !== richestPeople[index]) {
       listItem.classList.add("wrong");
     } else {
       listItem.classList.remove("wrong");
